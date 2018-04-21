dcc93a6baa58fab1b6f6dbc4f60377e002aaaf9c
JoJo Saunders <jojosaunders@D-173-250-163-126.dhcp4.washington.edu>
2018-04-20 18:36:15 -0700

    worked on exercise problems in and outside of class

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -2,30 +2,34 @@
 
 # Install the `nycflights13` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
-
-
+install.packages("nycflights13")
+library(nycflights13)
+library(dplyr)
 # The data frame `flights` should now be accessible to you.
 # Use functions to inspect it: how many rows and columns does it have?
 # What are the names of the columns?
 # Use `??flights` to search for documentation on the data set (for what the 
 # columns represent)
-
-
+View(flights)
+ncol(flights)
+nrow(flights)
 # Use `dplyr` to give the data frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
-
+flights <- flights %>%
+   mutate(time_gained = arr_delay - dep_delay)
 
 # Use `dplyr` to sort your data frame in descending order by the column you just
 # created. Remember to save this as a variable (or in the same one!)
-
+flights <- flights %>%
+   arrange(-time_gained)
 
 # For practice, repeat the last 2 steps in a single statement using the pipe
 # operator. You can clear your environmental variables to "reset" the data frame
 
 
 # Make a histogram of the amount of time gained using the `hist()` function
-
+hist(flights$time_gained)
 
 # On average, did flights gain or lose time?
 # Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
